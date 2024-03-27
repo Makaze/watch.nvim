269cbc4bde06e54f2eb02c660824b2469af3a153
Makaze <christopherslane.work@gmail.com>
2024-03-27 10:05:53 -0400

    Testing a new config style

--- lua/watch/init.lua
+++ lua/watch/init.lua
@@ -1,12 +1,41 @@
+--- @class watch.Watcher
+---
+--- @field command string The command to watch. Serves as the name of the watcher.
+--- @field refresh_rate integer The refresh rate for the watcher in milliseconds.
+--- @field bufnr integer The buffer number attached to the watcher.
+--- @field timer timer The buffer number attached to the watcher.
+
 local M = {}
 
 local A = vim.api
 
---- @type integer | nil
+--- Check if a buffer is visible
 ---
---- The buffer number to load into
-M.buf = nil
+--- @param bufnr integer The buffer number to check
+--- @return boolean visible
+local function visible(bufnr)
+    for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win_id) == bufnr then
+            return true
+        end
+    end
+    return false
+end
 
+-- --- @type integer | nil
+-- ---
+-- --- The buffer number to load into
+-- M.buf = nil
+-- --- @type integer | nil
+
+--- @type watch.Watcher[]
+---
+--- Global list of watchers and associated data
+M.watchers = {}
+
+--- Setup the plugin. Here for compability only.
+---
+--- @param opts table
 M.setup = function(opts)
     -- For compatability only
 end
@@ -14,9 +43,15 @@ end
 --- Replace buffer's contents with a shell command and preserve the cursor
 ---
 --- @param command string Shell command
+--- @param buf integer The buffer number to update
 --- @return function update Steps to take upon rerunning
-M.update = function(command)
+M.update = function(command, buf)
     return function()
+        -- Do nothing if not visible
+        if not visible(buf) then
+            return
+        end
+
         -- Save current cursor position
         local save_cursor = A.nvim_win_get_cursor(0)
 
@@ -31,7 +66,7 @@ M.update = function(command)
         end
 
         -- Clear the buffer and insert the stripped output
-        A.nvim_buf_set_lines(M.buf, 0, -1, false, stripped_output)
+        A.nvim_buf_set_lines(buf, 0, -1, false, stripped_output)
 
         -- Restore cursor position
         A.nvim_win_set_cursor(0, save_cursor)
@@ -42,7 +77,8 @@ end
 ---
 --- @param command string Shell command
 --- @param refresh_rate integer? Time between reloads in milliseconds. Default 500
-M.start = function(command, refresh_rate)
+--- @param buf integer? Buffer number to update. Default new buffe
+M.start = function(command, refresh_rate, buf)
     local uv = vim.loop or vim.uv
 
     -- Default to 500 ms
@@ -51,36 +87,46 @@ M.start = function(command, refresh_rate)
     end
 
     -- Create a new buffer
-    if not M.buf then
-        M.buf = A.nvim_create_buf(false, true)
+    if not buf then
+        buf = A.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_name(buf, command)
         -- Automatically delete buffer when no longer visible
-        vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = M.buf })
-        -- A.nvim_command "split"
-        A.nvim_win_set_buf(0, M.buf)
+        -- vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
+        A.nvim_win_set_buf(0, buf)
     end
 
     -- Set up a timer to run the function every 500ms
-    M.watch_timer = uv.new_timer()
-    M.watch_timer:start(
+    local timer = uv.new_timer()
+    timer:start(
         refresh_rate,
         refresh_rate,
-        vim.schedule_wrap(M.update(command))
+        vim.schedule_wrap(M.update(command, buf))
     )
+
+    --- @type watch.Watcher
+    local watcher = {
+        command = command,
+        bufnr = buf,
+        refresh_rate = refresh_rate,
+        timer = timer,
+    }
 
     local group = A.nvim_create_augroup("WatchCleanUp", { clear = true })
 
     -- Stop the timer when the buffer is unloaded or when quitting Neovim
     A.nvim_create_autocmd({ "BufUnload", "VimLeavePre" }, {
         group = group,
-        buffer = M.buf,
+        buffer = buf,
         callback = M.stop,
     })
 end
 
 --- Stop watching and detach from the buffer
-M.stop = function()
-    M.buf = nil
-    M.watch_timer:stop()
+M.stop = function(event)
+    vim.enotify(vim.inspect(event))
+    local command = vim.api.nvim_buf_get_name(event.buffer)
+
+    M.watchers[command].timer:stop()
 end
 
 return M
