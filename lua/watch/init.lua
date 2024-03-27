778a2685f4b8ba856ae1580b7fffede5456bd4d6
Makaze <christopherslane.work@gmail.com>
2024-03-27 12:25:48 -0400

    Change options to avoid adding path to bufname

--- lua/watch/init.lua
+++ lua/watch/init.lua
@@ -124,7 +124,7 @@ M.start = function(command, refresh_rate, buf)
         buf = A.nvim_create_buf(false, true)
         A.nvim_set_option_value("buftype", "nofile", { buf = buf })
         A.nvim_buf_set_name(buf, command)
-        vim.cmd("badd " .. command)
+        vim.fn.setbufvar(buf, "&buflisted", 1)
         -- A.nvim_set_option_value("buflisted", true, { buf = buf })
         A.nvim_win_set_buf(0, buf)
     end
